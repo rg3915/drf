b0aa989ffc756e6dc5f65e172dfb43d47127d743
rg3915 <rg3915@yahoo.com.br>
2018-02-14 03:23:17 -0200

    step-5 Rewriting our API using CBV
    - create PersonList
    - create PersonDetail
    - change urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from core import views
 
 urlpatterns = [
-    path('persons/', views.person_list),
-    path('persons/<int:pk>/', views.person_detail),
+    path('persons/', views.PersonList.as_view()),
+    path('persons/<int:pk>/', views.PersonDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- core/views.py
+++ core/views.py
@@ -1,21 +1,22 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from core.models import Person
 from core.serializers import PersonSerializer
 
 
-@api_view(['GET', 'POST'])
-def person_list(request):
+class PersonList(APIView):
     """
     List all persons, or create a new person.
     """
-    if request.method == 'GET':
+
+    def get(self, request, format=None):
         persons = Person.objects.all()
         serializer = PersonSerializer(persons, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = PersonSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -23,27 +24,31 @@ def person_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def person_detail(request, pk):
+class PersonDetail(APIView):
     """
     Retrieve, update or delete a person instance.
     """
-    try:
-        person = Person.objects.get(pk=pk)
-    except Person.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        person = self.get_object(pk)
         serializer = PersonSerializer(person)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        person = self.get_object(pk)
         serializer = PersonSerializer(person, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        person = self.get_object(pk)
         person.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
