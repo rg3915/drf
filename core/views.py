0d5822feab34ac4c897175673d10f7a4edffd311
rg3915 <rg3915@yahoo.com.br>
2018-02-14 03:31:22 -0200

    step-6 Using Mixins

--- core/views.py
+++ core/views.py
@@ -1,54 +1,34 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 from core.models import Person
 from core.serializers import PersonSerializer
 
 
-class PersonList(APIView):
-    """
-    List all persons, or create a new person.
-    """
+class PersonList(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
 
-    def get(self, request, format=None):
-        persons = Person.objects.all()
-        serializer = PersonSerializer(persons, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = PersonSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class PersonDetail(APIView):
-    """
-    Retrieve, update or delete a person instance.
-    """
+class PersonDetail(mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   generics.GenericAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
 
-    def get_object(self, pk):
-        try:
-            return Person.objects.get(pk=pk)
-        except Person.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        person = self.get_object(pk)
-        serializer = PersonSerializer(person)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        person = self.get_object(pk)
-        serializer = PersonSerializer(person, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        person = self.get_object(pk)
-        person.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
